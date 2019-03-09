6e3991307f7e4d2f9be3948139d1231440356115
Timothy Fisher <timothyf@gmail.com>
2019-03-09 14:28:26 -0500

    get player list and obas

--- js/base.js
+++ js/base.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
   $( "form" ).submit(function( event ) {
 
-    alert('test here');
+    var playerList = [];
+    $("form .player-name").each(function(index){
+      var player = {};
+      player["position"] = index+1;
+      player["name"] = $(this).val(); // This is the jquery object of the input, do what you will
+      player["oba"] = $(".player-oba:eq(" + index + ")").val();
+      playerList.push(player);
+    });
 
-    // get the list of players with OBA
-    //
-
-
-
+    processLineup(playerList);
 
     event.preventDefault();
   });
 });
 
 
-function processLineup(playersList) {
+function processLineup(playerList) {
+  alert('made it here');
+  alert(playerList.length);
   
 }
