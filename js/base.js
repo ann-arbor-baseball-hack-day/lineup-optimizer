71b461c7b88070a2f263da358bca5dbacac13e76
Timothy Fisher <timothyf@gmail.com>
2019-03-09 13:43:51 -0500

    updates

---
+++ js/base.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $( "form" ).submit(function( event ) {
+
+    alert('test here');
+
+    // get the list of players with OBA
+    //
+
+
+
+
+    event.preventDefault();
+  });
+});
+
+
+function processLineup(playersList) {
+  
+}
